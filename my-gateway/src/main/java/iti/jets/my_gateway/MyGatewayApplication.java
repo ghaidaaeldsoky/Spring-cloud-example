ce26c6d859fe4f79885d012ddfb4e423d9c39201
ghaidaaeldsoky <ghaidaaeldsoky@gmail.com>
2025-06-13 17:45:09 +0300

    add gateway routing

---
+++ my-gateway/src/main/java/iti/jets/my_gateway/MyGatewayApplication.java
@@ -0,0 +1,13 @@
+package iti.jets.my_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyGatewayApplication.class, args);
+	}
+
+}
---
+++ my-gateway/src/main/java/iti/jets/my_gateway/config/GatewayConfig.java
@@ -0,0 +1,20 @@
+package iti.jets.my_gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+    
+    @Bean
+    public RouteLocator myRouteLocator(RouteLocatorBuilder builder){
+        return builder.routes()
+        .route("user-app", r->r.path("/users/**").uri("lb://user-app"))
+        .route("order-app", r->r.path("/orders/**").uri("lb://order-app"))
+        .route("product-app", r->r.path("/products/**").uri("lb://product-app"))
+        .build();
+        
+    }
+}
